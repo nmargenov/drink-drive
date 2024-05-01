81b1f88f8a58ff038de68e0701b2ae08a099f29f
nmargenov <nikolay_margenov@yahoo.com>
2024-05-01 17:34:03 +0300

    deploy

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,56 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Header.module.css"
+import { faBars, faClose, faX, } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 export const Header = () =>{
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+    if(isNavOpen){
+        
+    }
+  };
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollTop = window.pageYOffset;
+
+      // Check if scrolled to the bottom
+      setIsScrolled(scrollTop > 100 );
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
     return(
-    <header>
+        <>
+    <header className={isScrolled ? styles["opaque"] : ""}>
         <div className={styles['logo-div']}>
             <img src='/images/logo.avif' alt="" />
         </div>
-        <nav>
+        <nav className={isNavOpen ? styles['visiable'] : ""}>
             <ul>
                 <li>Услуги</li>
                 <li>Информация</li>
                 <li>Цени</li>
             </ul>
         </nav>
+        <div className={styles['actions']}>
+            <FontAwesomeIcon onClick={toggleNav} id={styles["bars"]} icon={isNavOpen ? faX : faBars} />
+        </div>
     </header>
+    <div className={styles["background"]}>
+        <img src="/images/car.png" alt="" />
+    </div>
+    </>
 );
 }
