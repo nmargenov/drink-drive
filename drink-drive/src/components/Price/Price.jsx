8c866b08bde3f647c69de0715525590992208712
nmargenov <nikolay_margenov@yahoo.com>
2024-05-02 17:34:34 +0300

    Update Price.jsx

--- drink-drive/src/components/Price/Price.jsx
+++ drink-drive/src/components/Price/Price.jsx
@@ -7,13 +7,13 @@ export const Price = () =>{
             <div className={styles["price-container"]}>
                 <div className={styles['price']}>
                     <h1>Минимална цена за курс</h1>
-                    <h2>22.00Лв. с ДДС</h2>
+                    <h2>22.00лв. с ДДС</h2>
                     <p id={styles["para"]}>До 5км включително</p>
                 </div>
                 <div className={styles['price']}>
                     <h1>Цена на километър</h1>
-                    <h2>2,40 ЛВ</h2>
-                    <p id={styles["para"]}>+0,40 лв. на минута</p>
+                    <h2>2,40лв.</h2>
+                    <p id={styles["para"]}>+0,40лв. на минута</p>
                 </div>
                 <div className={styles['price']}>
                     <h1>Работно време</h1>
